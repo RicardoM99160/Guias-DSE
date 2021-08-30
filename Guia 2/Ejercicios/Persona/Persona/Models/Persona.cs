4c5f5c84667c7af493cdd3833ac86ac7029153e1
RicardoM <jmajano@gbm.net>
2021-08-29 18:34:49 -0600

    subiendo guia 2

---
+++ Guia 2/Ejercicios/Persona/Persona/Models/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Persona.Models
+{
+    public class Persona
+    {
+        public string DUI { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public string direccion { get; set; }
+        public string email { get; set; }
+    }
+
+    public class PersonaDBContext: DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
